0e6e832193e6b078144edf751198c0c34f7b0471
KAbaidoo <KAbaidoo@hotmail.com>
2020-10-22 06:00:52 +0000

    fixed Bookmark star type toggle

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ requirements
 Provide current weather conditions and forecast based on a city search
 1.  Detect location of user and provide weather conditions on start of application.
 2.  Enable user to bookmark a city to be loaded on next visit
-3. 
+3.  
 
 
 
@@ -14,14 +14,59 @@ Provide current weather conditions and forecast based on a city search
 
 const API_KEY = "c1749cb94a9a268e7a35ba3804755af0";
 const current = document.querySelector(".current");
-const btn = document.getElementById("btn");
 const input = document.getElementById("search")
 const bookmark = document.querySelector(".fa-star")
 
-window.addEventListener("DOMContentLoaded", () => {
-    getLocationCoords()
+function loadPage() {
+    // check for bookmarked city
+    let city = localStorage.getItem('bookmark')
+    if (!(city === null || city === "")) {
+        // load weather for bookmarked city
+        getCurrentWeatherByCity(city)
+            .then(displayCurrentWeather)
+        // bookmark.classList.remove("far")
+        // bookmark.classList.add("fas")
+    } else {
+        // if there are no bookmarked cities load weather by location coordinates
+        getLocationCoords()
+        // bookmark.classList.remove("fas")
+        // bookmark.classList.add("far")
+    }
+
+}
+
+
+window.addEventListener("DOMContentLoaded",
+    loadPage)
+
+
+
+const reloadBtn = document.querySelector(".fa-redo")
+//  Reload weather information
+reloadBtn.addEventListener("click",
+    loadPage
+)
+
+
+
+// Bookmark a city (app loads to bookmarked city on load)
+
+bookmark.addEventListener("click", (e) => {
+
+    if (localStorage.getItem('bookmark') === input.value) {
+        window.localStorage.removeItem('bookmark');
+        e.currentTarget.classList.remove("fas")
+        e.currentTarget.classList.add("far")
+
+    } else {
+        window.localStorage.setItem('bookmark', input.value);
+        e.currentTarget.classList.remove("far")
+        e.currentTarget.classList.add("fas")
+    }
 })
 
+
+// get weather based on location coordinates of user browser
 function getLocationCoords() {
     // check if the Geolocation API is supported
 
@@ -41,7 +86,7 @@ function getLocationCoords() {
     }
 }
 
-// get current weather of search city
+// get weather of search city
 input.addEventListener("keydown", function (e) {
     if (e.code === "Enter") {
         let city = input.value;
@@ -50,7 +95,7 @@ input.addEventListener("keydown", function (e) {
 })
 
 
-// Get current weather
+// Get weather data
 async function getCurrentWeatherByCoords(lat, lon) {
     try {
         let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
@@ -68,6 +113,8 @@ async function getCurrentWeatherByCity(city) {
     }
 }
 
+
+// display information
 let displayCurrentWeather = (data) => {
     let temp = data.main.temp.toPrecision(2);
     let feelsLike = data.main.feels_like.toPrecision(2);
@@ -93,83 +140,12 @@ let displayCurrentWeather = (data) => {
       </article>`;
     current.innerHTML = currentWeather;
 
-}
-/* function displayCurrentWeatherByCity(city) {
-    getCurrentWeatherByCity(city).then(
-        (data) => {
-            let temp = data.main.temp.toPrecision(2);
-            let feelsLike = data.main.feels_like.toPrecision(2);
-            let humidity = data.main.humidity;
-            let pressure = data.main.pressure;
-            let windSpeed = data.wind.speed;
-            // let windDeg = data.wind.deg;
-            let visibility = data.visibility;
-            let country = data.sys.country;
-            let city = data.name;
-            let arr = data.weather;
-            let desc = [];
-            arr.forEach(obj => {
-                desc.push(obj["main"]);
-            })
-            let results = desc.join(", ");
-            let currentWeather = `<article class="current">
-        <h1>${city}, ${country}</h1>
-        <h2><span>${temp}&nbsp;°C</span> <br>${results}</h2>
-        <p>Feels like ${feelsLike}&nbsp;°   &nbsp;&nbsp;    Wind ${windSpeed}&nbsp;km/h   &nbsp;&nbsp;    visibility ${visibility / 1000}&nbsp;km<br>
-        Pressure ${pressure}&nbsp;hPa   &nbsp;&nbsp;    Humidity ${humidity}&nbsp;%</p>
-
-      </article>`;
-            current.innerHTML = currentWeather;
-
+    // change bookmark icon if city is bookmarked
+    if (localStorage.getItem('bookmark')) {
+        let fav = localStorage.getItem('bookmark');
+        if (fav === city.toLowerCase()) {
+            bookmark.classList.remove("far")
+            bookmark.classList.add("fas")
         }
-    )
+    }
 }
- */
-
-// Get current weather for bookmarked city upon loading
-/* window.addEventListener("DOMContentLoaded", function () {
-    // console.log(latitude)
-    let city = localStorage.getItem('bookmark')
-
-    if (!(city === null || city === "")) {
-        displayCurrentWeather(city)
-        bookmark.classList.toggle("far")
-        bookmark.classList.toggle("fas")
-    }
-}) */
-
-// bookmark a city
-/* bookmark.addEventListener("click", (e) => {
-
-    if (localStorage.getItem('bookmark') === input.value) {
-        window.localStorage.removeItem('bookmark');
-
-        e.currentTarget.classList.toggle("fas")
-        e.currentTarget.classList.toggle("far")
-    } else {
-        window.localStorage.setItem('bookmark', input.value);
-        e.currentTarget.classList.toggle("far")
-        e.currentTarget.classList.toggle("fas")
-    }
-}) */
-
-// get location
-/* (() => {
-    // check if the Geolocation API is supported
-    if (!navigator.geolocation) {
-        alert(`Your browser doesn't support Geolocation`);
-        return;
-    }
-    navigator.geolocation.getCurrentPosition(onSuccess, onError);
-    // handle success case
-    function onSuccess(position) {
-        lat = position.coords.longitude;
-        lon = position.coords.longitude;
-        console.log(`Your location: (${lat},${lon})`);
-    }
-    // handle error case
-    function onError() {
-        alert(`Failed to get your location!`);
-    }
-})();
-*/
